1567253e6f632da033968f57df522679ef2a8f13
serkanktk <serkankutuk@sabanciuniv.edu>
2023-07-14 20:50:15 +0300

    object detection added

--- main.py
+++ main.py
@@ -2,9 +2,86 @@ import cv2
 import numpy as np
 import open3d as o3d
 from scipy.ndimage import uniform_filter
+import os
+
+
+#
+# Load YOLO
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+layer_names = net.getLayerNames()
+output_layers = net.getUnconnectedOutLayersNames()
+
+# Loading the labels
+classes = []
+with open("coco.names", "r") as file:
+    classes = [line.strip() for line in file.readlines()]
+
+# Classes for Bicycle, Car, Motorcycle, Bus, and Truck
+selected_classes = ['bicycle', 'car', 'motorcycle', 'bus', 'truck']
+
+# Loading video
+cap = cv2.VideoCapture('video_sources/traf_video_Trim.mp4')
+width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
+
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+output_video = cv2.VideoWriter('output.mp4', fourcc, 20.0, (int(width), int(height)))
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+    if not ret:
+        break
+
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5 and classes[class_id] in selected_classes:  # Only select classes specified
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 2)
+            cv2.putText(img, label, (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
+
+    # only write frame to video file if img is not None
+    if img is not None:
+        output_video.write(img)
+
+cap.release()
+output_video.release()
+cv2.destroyAllWindows()
+
 
 # Load the video and get the first frame
-cap = cv2.VideoCapture(r"C:\Users\ipekd\ENS491-RealityCapture-1\video_sources\traf_video_Trim.mp4")
+cap = cv2.VideoCapture(r"video_sources\traf_video_Trim.mp4")
 ret, frame1 = cap.read()
 
 # Check if the first frame was successfully read
@@ -60,6 +137,7 @@ while cap.isOpened():
     # Set the first frame to be the second frame for the next iteration
     frame1 = frame2.copy()
 
+    """
     # At the end of each loop iteration
     if len(good_old) > 0:
         # Draw the good features on the frame
@@ -74,10 +152,11 @@ while cap.isOpened():
 
         # Resize and show the frame
         cv2.imshow('Feature Points', cv2.resize(frame1, (1800, int(frame1.shape[0] * 1800 / frame1.shape[1]))))
-
+    
     # Break if the user presses the 'q' key
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+    """
 
 # Release the video capture object
 cap.release()
@@ -108,3 +187,13 @@ pcd.colors = o3d.utility.Vector3dVector(colors[indices][:, ::-1] / 255)
 
 # Show the point cloud
 o3d.visualization.draw_geometries([pcd])
+
+
+
+
+# pip install opencv-python
+# pip install open3d
+# pip install scipy
+# pip install numpy
+
+
