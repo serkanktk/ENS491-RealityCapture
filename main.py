fdab704d2f904d39344b3723379c083eaec6f7a8
serkanktk <128151657+serkanktk@users.noreply.github.com>
2023-10-02 17:22:49 +0300

    Farneback's method for dense optical method added
    Farneback's method is better than the Lucas-Kanade method for moving objects. Therefore, the optical calculation method is updated.

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ from time import time
 # camera calibration and camera pose estimation
 
 
-
+"""
 class ObjectDetection:
-    """
+    
     Class implements Yolo5 model to make inferences on a local video using OpenCV.
-    """
+    
     
     def __init__(self, file_path, out_file):
-        """
+        
         Initializes the class with local video file path and output file.
         :param file_path: Path to the local video file on which prediction is made.
         :param out_file: A valid output file name.
-        """
+        
         self._file_path = file_path
         self.model = self.load_model()
         self.classes = self.model.names
@@ -41,28 +41,28 @@ class ObjectDetection:
 
 
     def get_video(self):
-        """
+        
         Opens the local video file using OpenCV.
         :return: opencv2 video capture object.
-        """
+        
         return cv2.VideoCapture(self._file_path)
 
 
     def load_model(self):
-        """
+        
         Loads Yolo5 model from pytorch hub.
         :return: Trained Pytorch model.
-        """
+        
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         return model
 
 
     def score_frame(self, frame):
-        """
+        
         Takes a single frame as input, and scores the frame using yolo5 model.
         :param frame: input frame in numpy/list/tuple format.
         :return: Labels and Coordinates of objects detected by model in the frame.
-        """
+        
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
@@ -72,21 +72,21 @@ class ObjectDetection:
 
 
     def class_to_label(self, x):
-        """
+        
         For a given label value, return corresponding string label.
         :param x: numeric label
         :return: corresponding string label
-        """
+        
         return self.classes[int(x)]
 
 
     def plot_boxes(self, results, frame):
-        """
+        
         Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
         :param results: contains labels and coordinates predicted by model on the given frame.
         :param frame: Frame which has been scored.
         :return: Frame with bounding boxes and labels plotted on it.
-        """
+        
         labels, cord = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
@@ -106,11 +106,11 @@ class ObjectDetection:
 
 
     def __call__(self):
-        """
+        
         This function is called when class is executed, it runs the loop to read the video frame by frame,
         and write the output into a new file.
         :return: void
-        """
+        
         player = self.get_video()
         assert player.isOpened()
         x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -136,27 +136,34 @@ class ObjectDetection:
 video_path = r"Dataset\Data1\video1.mp4"
 detection = ObjectDetection(video_path, "data1_video1_output.avi")
 detection()
-
-
+"""
 
 
 # Load the video and get the first frame
-cap = cv2.VideoCapture(r"video_sources\traf_video_Trim.mp4")
+cap = cv2.VideoCapture(r"StartToEnd.mp4")
 ret, frame1 = cap.read()
+
+
+
+all_depths = [] # DELETE LATER___________________________________________________________________________________________
+all_keypoints = []
+
+
+
 
 # Check if the first frame was successfully read
 if not ret:
     print("Error reading the first frame from the video.")
     exit(1)
 
-# Create a FAST feature detector
+# Create a FAST feature detector___________________________________________________________________________________________________________________________
 fast = cv2.FastFeatureDetector_create(threshold=20, nonmaxSuppression=True)
 
-# Create some empty arrays to store the points and their corresponding depth
-p1 = np.empty((0, 2))
-p2 = np.empty((0, 2))
-depths = np.empty((0))
+# Instead of initializing empty arrays for keypoints, initialize dense flow arrays
+flow = None
+hsv = np.zeros_like(frame1)
 
+# Loop through the video frame by frame
 # Loop through the video frame by frame
 while cap.isOpened():
     ret, frame2 = cap.read()
@@ -169,77 +176,80 @@ while cap.isOpened():
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    # Detect keypoints using FAST in the first frame
-    keypoints_1 = fast.detect(gray1, None)
+    # Compute dense optical flow using Farneback method
+    flow = cv2.calcOpticalFlowFarneback(gray1, gray2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-    # Extract the keypoint locations
-    p_0 = np.array([kp.pt for kp in keypoints_1], dtype=np.float32)
+    # Convert flow to polar coordinates to get magnitude and angle
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
 
-    # Calculate the optical flow between the first and second frames
-    p_1, st, err = cv2.calcOpticalFlowPyrLK(gray1, gray2, p_0, None)
-
-    # Select only good points
-    good_new = p_1[st[:, 0] == 1]
-    good_old = p_0[st[:, 0] == 1]
-
-    # Calculate the depth of each point based on the motion of the camera
-    # Calculate the depth of each point based on the motion of the camera
-    depth = np.linalg.norm(good_new - good_old, axis=1)
-    # Apply a moving average filter to the depth estimates
-    window_size = 3  # or whatever window size you want
-    depth = uniform_filter(depth, size=window_size, mode='reflect')
-
-    # Apply the Kalman filter to the depth estimates  # Add this line to apply KalmanFilter
-    # smoothed_depth, _ = kf.smooth(depth)
+    # Extracting meaningful keypoints from the dense flow
+    threshold_value = 2.0  # you can adjust this value_____________________________________________________________--ADJUST
+    keypoints_y, keypoints_x = np.where(mag > threshold_value)
+    keypoints = np.stack((keypoints_x, keypoints_y), axis=-1).astype(np.float32)
 
 
 
-    # Append the good points and their depth to the arrays
-    p2 = np.vstack([p2, good_new.reshape(-1, 2)])
-    p1 = np.vstack([p1, good_old.reshape(-1, 2)])
-    depths = np.append(depths, depth)
-    # depths = np.append(depths, np.full((good_new.shape[0],), smoothed_depth[:, 0].mean()))  # Use smoothed_depth instead of depth
+     # Visualize keypoints on the frame
+    vis_frame = frame2.copy()
+    for pt in keypoints:
+        cv2.circle(vis_frame, (int(pt[0]), int(pt[1])), 3, (0, 255, 0), -1)  # green color for the points
 
+    cv2.imshow('Key Points', vis_frame)
+    cv2.waitKey(1)  # Display the frame for a short time; you can adjust this delay if needed
+
+
+
+
+
+
+
+    # Append these keypoints for later use
+    all_keypoints.append(keypoints)
+
+    # Convert keypoints to integer for indexing
+    int_keypoints = keypoints.astype(np.int32)
+
+    # Corresponding depths (from the magnitude) for these keypoints
+    current_depths = mag[int_keypoints[:, 1], int_keypoints[:, 0]]
+    all_depths.append(current_depths)
+
+    # Visualization (optional)
+    hsv = np.zeros((frame1.shape[0], frame1.shape[1], 3), dtype=np.uint8)
+    hsv[..., 0] = ang * 180 / np.pi / 2
+    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    # You can display this visualization using cv2.imshow() if needed.
 
     # Set the first frame to be the second frame for the next iteration
     frame1 = frame2.copy()
 
     """
-    # At the end of each loop iteration
-    if len(good_old) > 0:
-        # Draw the good features on the frame
-        for i, point in enumerate(good_old):
-            point = (int(point[0]), int(point[1]))
-            cv2.circle(frame1, point, 5, (0, 0, 255), -1)
-
-            # Draw the depth value next to the point
-            depth = depths[i]
-            cv2.putText(frame1, f'depth: {depth:.2f}', (point[0] + 5, point[1] + 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (0, 0, 255), 2, cv2.LINE_AA)
-
-        # Resize and show the frame
-        cv2.imshow('Feature Points', cv2.resize(frame1, (1800, int(frame1.shape[0] * 1800 / frame1.shape[1]))))
-    
-    # Break if the user presses the 'q' key
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    Depth Display
+    do not delete this comment section
     """
 
 # Release the video capture object
 cap.release()
 cv2.destroyAllWindows()
 
-# Convert the arrays to the correct format
-p1 = np.round(p1).astype(int)
+# Concatenate the keypoints and depths for final processing
+keypoints = np.concatenate(all_keypoints)
+depths = np.concatenate(all_depths)
 depths = depths[:, np.newaxis]
 
-# Ensure p1 values do not exceed image dimensions
+
+# Assuming 'keypoints' and 'depths' are populated correctly from the loop.
+
+# Convert the keypoints array to the correct format
+keypoints = np.round(keypoints).astype(int)
+
+# Ensure keypoints values do not exceed image dimensions
 height, width, _ = frame1.shape
-p1[:, 0] = np.clip(p1[:, 0], 0, width-1)
-p1[:, 1] = np.clip(p1[:, 1], 0, height-1)
+keypoints[:, 0] = np.clip(keypoints[:, 0], 0, width-1)
+keypoints[:, 1] = np.clip(keypoints[:, 1], 0, height-1)
 
 # Convert 2D points to 1D indices
-indices = p1[:, 1] * width + p1[:, 0]
+indices = keypoints[:, 1] * width + keypoints[:, 0]
 
 # Reshape the image to a 2D array of colors
 colors = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
@@ -249,11 +259,12 @@ colors = colors.reshape(-1, 3)
 pcd = o3d.geometry.PointCloud()
 
 # Set the points and colors of the point cloud
-pcd.points = o3d.utility.Vector3dVector(np.hstack((p1, depths)))
+pcd.points = o3d.utility.Vector3dVector(np.hstack((keypoints, depths)))
 pcd.colors = o3d.utility.Vector3dVector(colors[indices][:, ::-1] / 255)
 
 # Show the point cloud
 o3d.visualization.draw_geometries([pcd])
+
 
 
 
@@ -281,3 +292,25 @@ and then find the correct PyTorch version for your hardware:
 
 """
 
+"""
+    -- Depth Display --
+
+    # At the end of each loop iteration
+    if len(good_old) > 0:
+        # Draw the good features on the frame
+        for i, point in enumerate(good_old):
+            point = (int(point[0]), int(point[1]))
+            cv2.circle(frame1, point, 5, (0, 0, 255), -1)
+
+            # Draw the depth value next to the point
+            depth = depths[i]
+            cv2.putText(frame1, f'depth: {depth:.2f}', (point[0] + 5, point[1] + 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 0, 255), 2, cv2.LINE_AA)
+
+        # Resize and show the frame
+        cv2.imshow('Feature Points', cv2.resize(frame1, (1800, int(frame1.shape[0] * 1800 / frame1.shape[1]))))
+    
+    # Break if the user presses the 'q' key
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+    """
